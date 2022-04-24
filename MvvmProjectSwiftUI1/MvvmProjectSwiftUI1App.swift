a0507bd5a76bfaccad7739862e18afeed9b491f2
Pedro Luis <pedroluisnr22@gmail.com>
2022-04-23 23:00:07 -0500

    Initial Commit

---
+++ MvvmProjectSwiftUI1/MvvmProjectSwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  MvvmProjectSwiftUI1App.swift
+//  MvvmProjectSwiftUI1
+//
+//  Created by Pedro Luis on 23/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MvvmProjectSwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

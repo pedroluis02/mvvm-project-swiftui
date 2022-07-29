4b731f92e77fa96b90130d88ee4e95f9549907c7
Pedro Luis <pedroluisnr22@gmail.com>
2022-07-29 16:13:15 -0500

    Added app state handler

--- MvvmProjectSwiftUI1/MvvmProjectSwiftUI1App.swift
+++ MvvmProjectSwiftUI1/MvvmProjectSwiftUI1App.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct MvvmProjectSwiftUI1App: App {
+    @Environment(\.scenePhase) var scenePhase
+    
+    init() {
+        print("App init method")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .active:
+                print("App is active")
+            case .inactive:
+                print("App is inacative")
+            case .background:
+                print("App is in background")
+            @unknown default:
+                print("Unknown app state")
+            }
         }
     }
 }
